409fdbc5ceb105ac857915d02e9d0727391120e5
Ryan Kraay <rk.no-reply@kurses.com>
2025-10-27 15:14:23 +0100

    Add failing unit test

--- spec/k6haze_spec.sh
+++ spec/k6haze_spec.sh
@@ -4,10 +4,19 @@ Describe "Example specfile"
       echo # "hello $1"
     }
 
+    add() {
+      echo "$1 + $2 + 1" | bc
+    }
+
     It "puts greeting, but not implemented"
       Pending "You should implement hello function"
       When call hello world
       The output should eq "hello world"
+    End
+
+    It "calculates the sum of two numbers"
+      When call add 3 2
+      The output should eq "5"
     End
   End
 End
